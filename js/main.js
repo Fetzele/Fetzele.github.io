b79a1f919b0331c7b9ef0f8e0302d5e96f8ae9e2
Johannes <johannes.fessele@gmail.com>
2020-05-17 13:04:57 +0200

    Remove mobile menu for now until it is needed

--- js/main.js
+++ js/main.js
@@ -65,19 +65,20 @@
 	  	});
 	}
 
-	var offcanvasMenu = function() {
-		$('body').prepend('<div id="fh5co-offcanvas" />');
-		$('body').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>');
+	// uncomment the following again if the menu in mobile shall be visible again
+	// var offcanvasMenu = function() {
+	// 	$('body').prepend('<div id="fh5co-offcanvas" />');
+	// 	$('body').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>');
 
-		$('.fh5co-main-nav .fh5co-menu-1 a, .fh5co-main-nav .fh5co-menu-2 a').each(function(){
+	// 	$('.fh5co-main-nav .fh5co-menu-1 a, .fh5co-main-nav .fh5co-menu-2 a').each(function(){
 
-			var $this = $(this);
+	// 		var $this = $(this);
 
-			$('#fh5co-offcanvas').append($this.clone());
+	// 		$('#fh5co-offcanvas').append($this.clone());
 
-		});
-		// $('#fh5co-offcanvas').append
-	};
+	// 	});
+	// 	// $('#fh5co-offcanvas').append
+	// };
 
 	var mainMenuSticky = function() {
 		
